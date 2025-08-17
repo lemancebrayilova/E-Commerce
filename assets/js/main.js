c5a1db3372d77cb1873c940ff242b02ec14b6c57
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-17 13:56:59 +0300

    Nav hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,11 @@ function scrollTopBack() {
 scrollTopBack();
 
 // Nav Hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+
+navBar.forEach(function(a) {
+    a.addEventListener("click", function() {
+        navCollapse.classList.remove("show");
+    })
+})
