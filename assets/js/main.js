825680fc68b8e15c0dc15b3ec6ecad62e9fb8d85
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-17 13:54:09 +0300

    Back to top JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,5 +24,20 @@ countDown();
 setInterval(countDown, 1000);
 
 // Scroll Back To Top
+function scrollTopBack() {
+    let scrollTopButton = document.querySelector('#scrollUp');
+
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+
+        if(scroll >= 250) {
+            scrollTopButton.classList.add('scrollActive');
+        } else {
+            scrollTopButton.classList.remove('scrollActive');
+        }
+    }
+}
+
+scrollTopBack();
 
 // Nav Hide
